62b327987defeb27cce7a8c311ffb3025189b79f
Diarit-S <diarit-94@hotmail.fr>
2019-01-30 21:01:29 +0100

    animations fixed

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+var bubbles = document.querySelectorAll('.center__bubble');
+var enter = document.querySelector('.button');
+var logo = document.querySelector('.logo')
+var photo = document.querySelector('.photo');
+var texts = document.querySelectorAll('p');
+
+function hid() {
+  bubbles.forEach(bubble => {
+    bubble.classList.add('hidden');
+  });
+  photo.classList.add('visible');
+
+  texts.forEach(text => {
+    text.classList.add('valid');
+  })
+  
+}
+
+
+enter.addEventListener('click', function () {
+  
+  setTimeout(hid, 500);
+  logo.classList.add('zoom');
+  enter.classList.add('hidden');
+  
+});
