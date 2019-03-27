23b4acf91afe0a0faef3e3279b323fd1a3fd1a5e
Diarit-S <diarit-94@hotmail.fr>
2019-03-27 22:47:03 +0100

    p5js problem almost fixed

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,59 +1,65 @@
-var tousMesObjets = [];
+// import p5 from '../../node_modules/p5';
+
+const p5 = require('../../node_modules/p5');
 
 
-function setup() {
-  console.log('setup')
-  createCanvas(window.innerWidth, window.innerHeight);
-  noStroke();
-  var canvas_cursor = document.getElementById('defaultCanvas0');
-  canvas_cursor.style.position = "fixed";
-  canvas_cursor.style.zIndex = '-2';
-}
-  
-function draw() { 
-  background('rgba(47,42,56, 1)');
-  fill(255);
-  //ellipse(mouseX, mouseY, 6,6)
-   //text(tousMesObjets.length+"...", 50, 50);
-  var newObjets = [];
-  for (a=0; a<tousMesObjets.length; a++) { 
- // text(a+"...", 50, 50);
-    tousMesObjets[a].dessine();
-   if (tousMesObjets[a].opacite>10)newObjets.push(tousMesObjets[a]);
+var s = function(sketch) {
+  var tousMesObjets = [];
+
+  sketch.setup = function () {
+    console.log('setup');
+    sketch.createCanvas(window.innerWidth, window.innerHeight);
+    sketch.noStroke();
+    var canvas_cursor = document.getElementById('defaultCanvas0');
+    canvas_cursor.style.position = "fixed";
+    canvas_cursor.style.zIndex = '-4';
+    canvas_cursor.style.top = '0';
   }
- tousMesObjets =  newObjets ;
-}
-function mouseMoved() {
-  if (dist(mouseX, mouseY, pmouseX, pmouseY)>6) {
-    new objet();
-  }
-}
 
-function objet() {  
-  this.opacite=255;
-  this.t=6;
-  this.x=mouseX;
-  this.y=mouseY +0.1;
-  angle=random(TWO_PI); 
-  this.angleT = random(TWO_PI);
-  this.vangle = random(-PI, PI)*0.05;
-  this.vx = cos(angle)*1.4;
-  this.vy = sin(angle)*1.4;
-  tousMesObjets.push(this);
+  sketch.draw = function () {
+    sketch.background('rgba(0,0,0, 1)');
+    sketch.fill(255);
+    var newObjets = [];
+    for (a=0; a<tousMesObjets.length; a++) { 
+      tousMesObjets[a].dessine();
+      if (tousMesObjets[a].opacite>10)newObjets.push(tousMesObjets[a]);
+    }
+    tousMesObjets =  newObjets ;
+  };
 
-  this.dessine = function() {
+
+  sketch.mouseMoved = function() {
+    if (sketch.dist(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY)>6) {
+      new sketch.objet();
+    }
+  };
+
+  sketch.objet = function () {
+    this.opacite=255;
+    this.t=6;
+    this.x=sketch.mouseX;
+    this.y=sketch.mouseY +0.1;
+    angle=sketch.random(sketch.TWO_PI); 
+    this.angleT = sketch.random(sketch.TWO_PI);
+    this.vangle = sketch.random(-sketch.PI, sketch.PI)*0.05;
+    this.vx = sketch.cos(angle)*1.4;
+    this.vy = sketch.sin(angle)*1.4;
+    tousMesObjets.push(this);
+
+    this.dessine = function() {
     
-     this.opacite*=0.99;
-     this.x+= this.vx;
-     this.y+= this.vy;
-     this.t*=0.98;
-    fill(255,  this.opacite);
-   // fill(255);
-     this.angleT+= this.vangle;
-   // pushMatrix();
-    translate( this.x,  this.y);
-    rotate( this.angleT);
-    triangle(- this.t, 0,  this.t, 0, 0,  this.t);
-   resetMatrix();
-  }
-}
+      this.opacite*=0.99;
+      this.x+= this.vx;
+      this.y+= this.vy;
+      this.t*=0.98;
+      sketch.fill(255,  this.opacite);
+      this.angleT+= this.vangle;
+      sketch.translate( this.x,  this.y);
+      sketch.rotate( this.angleT);
+      sketch.triangle(- this.t, 0,  this.t, 0, 0,  this.t);
+      sketch.resetMatrix();
+    };
+  };
+};
+
+var myp5 = new p5(s);
